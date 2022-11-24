b71b27ae0e32c6cdcf622b18296079020a604e9a
dancun616 <dancun616@gmail.com>
2022-11-24 10:09:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
+// This block of code returns a random number between 1 and 10
+const random = Math.floor(Math.random()*10) + 1
+console.log(random)
+// calculates a remainder that is equal to 4
+let num3 = 16
+let num4 = 4
+const mod = num3 / num4
+console.log(mod)
+// return 20 as the highest number in the set
+const max = 20
+console.log(Math.max(max))
